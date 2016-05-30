811f1516e24c19edebf2f6e87790bb78aa8c0734
dmytrol <dmytrol@wix.com>
2016-05-30 19:41:22 +0300

    SeaBattleGame v.1.0

--- seabattlegame/seabattlegame.py
+++ seabattlegame/seabattlegame.py
@@ -40,6 +40,26 @@ def user_set(f, sign):
     else:
         f[row][column] += sign
 
+    for row in range(len(f)):
+        for item in range(len(f)):
+            while f[row][item] == sign * 2:
+                f[row][item] = sign
+                print("You've chosen already occupied spot! NOT GOOD!")
+                column = input("Enter a COLUMN (A, B or C): ").upper()
+                row = int(input("Enter a ROW (1, 2 or 3): "))
+                print()
+
+                if column == 'A':
+                    column = 1
+                elif column == 'B':
+                    column = 2
+                elif column == "C":
+                    column = 3
+
+                if f[row][column] == " ":
+                    f[row][column] = sign
+                else:
+                    f[row][column] += sign
     return f
 
 
@@ -59,6 +79,27 @@ def machine_set(f, sign):
     elif machine_column == 3:
         machine_column = 'C'
     print("Machine has chosen: " + machine_column + str(machine_row))
+
+    for row in range(len(f)):
+        for item in range(len(f)):
+            while f[row][item] == sign * 2:
+                f[row][item] = sign
+                print("Machine has chosen already occupied spot! NOT GOOD!. Trying again...")
+                machine_column = random.randint(1, 3)
+                machine_row = random.randint(1, 3)
+
+                if f[machine_row][machine_column] == " ":
+                    f[machine_row][machine_column] = sign
+                else:
+                    f[machine_row][machine_column] += sign
+
+                if machine_column == 1:
+                    machine_column = 'A'
+                elif machine_column == 2:
+                    machine_column = 'B'
+                elif machine_column == 3:
+                    machine_column = 'C'
+                print("Machine has chosen: " + machine_column + str(machine_row))
 
     return f
 
@@ -117,5 +158,4 @@ while True:
 
     # print("Check if won: ")
     if not is_hit(f_user, PLAYER_MACHINE):
-        print("MILK!")
         print_field(f_user)
