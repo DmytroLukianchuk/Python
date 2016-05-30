348dd186902e012a2c07b41fe737179fdaf86ae1
dmytrol <dmytrol@wix.com>
2016-05-30 20:10:28 +0300

    SeaBattleGame v.1.0

--- seabattlegame/seabattlegame.py
+++ seabattlegame/seabattlegame.py
@@ -1,5 +1,6 @@
 __author__ = 'dmytrol'
 import random
+
 BOAT_SIGN = '#'
 MILK_SIGN = 'O'
 HIT_SIGN = 'X'
@@ -21,6 +22,20 @@ def print_field(f):
     for row in f:
         print(line.join(row), "|")
         print('*' * 15)
+
+
+def print_field_for_user(f):
+    line = ' | '
+    for i in range(len(f)):
+        for j in range(len(f)):
+            if f[i][j] == BOAT_SIGN:
+                f[i][j] = " "
+
+    for row in f:
+        print(line.join(row), "|")
+        print('*' * 15)
+
+    return f
 
 
 def user_set(f, sign):
@@ -78,7 +93,9 @@ def machine_set(f, sign):
         machine_column = 'B'
     elif machine_column == 3:
         machine_column = 'C'
-    print("Machine has chosen: " + machine_column + str(machine_row))
+
+    if not sign == BOAT_SIGN:
+        print("Machine has chosen: " + machine_column + str(machine_row))
 
     for row in range(len(f)):
         for item in range(len(f)):
@@ -100,7 +117,6 @@ def machine_set(f, sign):
                 elif machine_column == 3:
                     machine_column = 'C'
                 print("Machine has chosen: " + machine_column + str(machine_row))
-
     return f
 
 
@@ -110,11 +126,10 @@ def is_hit(f, player):
             if f[row][item] == BOAT_SIGN + MILK_SIGN:
                 f[row][item] = HIT_SIGN
                 print("BOOM!", player, "has hit")
-                print_field(f)
+                print_field_for_user(f)
                 quit()
             if f[row][item] == MILK_SIGN * 2:
                 f[row][item] = MILK_SIGN
-                print("MILK!")
                 return False
     return False
 
@@ -135,10 +150,9 @@ user_set(f_user, BOAT_SIGN)
 print_field(f_user)
 print()
 
-print("Machine has set up the boat: ")
+print("Machine has set up the boat. Try to guess where it is! :) ")
 f_machine = field_gen()
 machine_set(f_machine, BOAT_SIGN)
-print_field(f_machine)
 print()
 
 print("Game Began!")
@@ -149,8 +163,8 @@ while True:
     game_turn(PLAYER_USER, f_machine)
 
     if not is_hit(f_machine, PLAYER_USER):
-        print("MILK!")
-        print_field(f_machine)
+        print("MILK! Machine's field")
+        print_field_for_user(f_machine)
 
     print()
     print("Enemy shoots you: ")
@@ -158,4 +172,6 @@ while True:
 
     # print("Check if won: ")
     if not is_hit(f_user, PLAYER_MACHINE):
+        print()
+        print("MILK! Your field")
         print_field(f_user)
